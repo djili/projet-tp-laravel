f6f9aa64f544ef4d3b409c60edd3d0dd055d295d
diopous1@gmail.com <cibercafe>
2018-07-31 22:42:56 +0000

    correction de etudiant controller

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Etudiant;
+use Illuminate\Http\Request;
+
+class EtudiantController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function add(Request $request){
+
+        Etudiant::create($request->all());
+        return back()->with('status',trans('etudiant.msgenregistrementok'));
+
+    }
+
+    public function show($id){
+
+        $etudiant = Etudiant::findorfail($id);
+        return view('etudiant.show',compact('etudiant'));
+
+    }
+
+    public function edit($id){
+
+        $etudiant = Etudiant::findorfail($id);
+        return view('etudiant.edit',compact('etudiant'));
+
+    }
+
+    public function update(Request $request, $id){
+
+        $etudiant = Etudiant::findorfail($id);
+        $etudiant->nom = $request->input('nom');
+        $etudiant->prenom = $request->input('prenom');
+        $etudiant->save();
+        return back()->with('status',trans('etudiant.msgmiseajourok'));
+
+    }
+}
